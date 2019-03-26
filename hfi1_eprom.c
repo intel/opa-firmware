intel/opa-firmware

commit 0fdbe2418159dc09285f16659c68109a3dfc492c

hfi1_eprom: Fix filenames when generating multiple files
Commit 43445f0b0936 ("hfi1_eprom: separate files for collective
reads" ) introduced a change to handle repeating filenames when
dealing with systems that have multiple devices.

However, that change is broken in instances when the tool is
writing multiple files for the same device. Consequently, the
command 'hfi1_eprom -c config -b bulk -o op -r' would end up
producing files named 'config', 'bulk_1' and 'op_2' despite the
fact that the tool might be operating on a singe device.

This was due to the fact that the change above used a single
global variable that was being incremented for every file generated
without checking whether the file existed first.

The fix is to first check for the existence of the target file. If
it does exist, use the next index up. This handles not only operating
on multiple devices but also overwriting files from previous
invocations of the tool.

Fixes: 43445f0b0936 ("hfi1_eprom: separate files for collective reads")
Reviewed-by: John Fleck <john.fleck@intel.com>
Reviewed-by: Ira Weiny <ira.weiny@intel.com>
Signed-off-by: Mitko Haralanov <mitko.haralanov@intel.com>

hfi1_eprom.c
@@ -256,8 +256,6 @@ const char *command;		/* derived command name */
 uint32_t dev_id;		/* EEPROM device identification */
 uint32_t dev_mbits;		/* device megabit size */
 int verbose;
-int read_op_cnt = 0;
-char tmp_fname[PATH_MAX];
 bool silence_warnings = false;
 bool print_meta = false;
 bool service_mode = false;
@@ -1264,11 +1262,19 @@ void prepare_file(int op, int partition, const char *fname,
 	ssize_t nread;
 	int flags;
 	mode_t mode;
+	struct stat st;
+	int read_op_cnt = 0;
 
 	fi->name = fname;
-	if(op == DO_READ && read_op_cnt++ > 0)
+	if(op == DO_READ)
 	{
-		snprintf(tmp_fname, ARRAY_SIZE(tmp_fname), "%s_%d", fname, read_op_cnt-1);
+		char tmp_fname[PATH_MAX];
+
+		strncpy(tmp_fname, fname, ARRAY_SIZE(tmp_fname)-1);
+		tmp_fname[ARRAY_SIZE(tmp_fname)-1] = '\0';
+		while (!stat(tmp_fname, &st))
+			snprintf(tmp_fname, ARRAY_SIZE(tmp_fname), "%s_%d",
+				 fname, read_op_cnt++);
 		fi->name = tmp_fname;
 	}
 

commit ea7dc0a1250fdf024c4edb01ec09020a16039655

hfi1_eprom: Fix Klockwork issue
Klockwork detected the following issue with commit
0fdbe24 ("hfi1_eprom: Fix filenames when generating multiple
files"):

    [313] Address of a local variable is returned through formal
    argument 'fi->name'. Also there is one similar error on line 1365.

This commit address those issues by ensuring proper handling of
the filenames generated by the tool.

Fixes: 0fdbe24 ("hfi1_eprom: Fix filenames when generating multiple files")
Reviewed-by: John Fleck <john.fleck@intel.com>
Reviewed-by: Ira Weiny <ira.weiny@intel.com>
Signed-off-by: Mitko Haralanov <mitko.haralanov@intel.com>

hfi1_eprom.c
@@ -139,7 +139,7 @@ void *reg_mem = NULL;
 const char pci_eprom_device[] = "0x24f0";
 
 struct file_info {
-	const char *name;	/* name for the file */
+	char *name;	        /* name for the file */
 	size_t fsize;		/* file size, in bytes */
 	size_t bsize;		/* buffer size, in bytes */
 	void *buffer;		/* memory allocated buffer */
@@ -518,6 +518,7 @@ void do_write(int dev_fd, struct file_info *fi);
 void do_read(int dev_fd, struct file_info *fi);
 void prepare_file(int op, int partition, const char *fname,
 		  struct file_info *fi);
+void clean_file(struct file_info *fi);
 
 /*
  * Alternative erase range routine that does not use block erase commands.
@@ -544,6 +545,7 @@ void erase_range_slow(int dev_fd, uint32_t start, uint32_t len)
 
 	memset(tmp_fi.buffer + start, 0xff, len);
 	do_write(dev_fd, &tmp_fi);
+	clean_file(&tmp_fi);
 }
 #endif
 
@@ -1265,17 +1267,26 @@ void prepare_file(int op, int partition, const char *fname,
 	struct stat st;
 	int read_op_cnt = 0;
 
-	fi->name = fname;
-	if(op == DO_READ)
-	{
-		char tmp_fname[PATH_MAX];
+	if (fname) {
+		if(op == DO_READ)
+		{
+			char tmp_fname[PATH_MAX] = { 0 };
 
-		strncpy(tmp_fname, fname, ARRAY_SIZE(tmp_fname)-1);
-		tmp_fname[ARRAY_SIZE(tmp_fname)-1] = '\0';
-		while (!stat(tmp_fname, &st))
-			snprintf(tmp_fname, ARRAY_SIZE(tmp_fname), "%s_%d",
-				 fname, read_op_cnt++);
-		fi->name = tmp_fname;
+			strncpy(tmp_fname, fname, ARRAY_SIZE(tmp_fname)-1);
+			while (!stat(tmp_fname, &st))
+				snprintf(tmp_fname, ARRAY_SIZE(tmp_fname) - 1,
+					 "%s_%d", fname, read_op_cnt++);
+			fi->name = strdup(tmp_fname);
+		} else {
+			fi->name = strdup(fname);
+		}
+		if (!fi->name) {
+			fprintf(stderr,
+				"Unable to allocate memory for filename\n");
+			exit(1);
+		}
+	} else {
+		fi->name = NULL;
 	}
 
 	fi->part = partition;
@@ -1380,6 +1391,16 @@ void prepare_file(int op, int partition, const char *fname,
 
 		add_magic_bits_to_image(sbuf.st_size, fi->bsize, fi->buffer);
 	}
+}
+
+void clean_file(struct file_info *fi)
+{
+	if (fi->name)
+		free(fi->name);
+	if (fi->fd >= 0)
+		close(fi->fd);
+	if (fi->buffer)
+		free(fi->buffer);
 }
 
 char *operation_str(int operation)
@@ -1491,10 +1512,7 @@ void do_operation(int operation, int dev_fd, int partition, const char *fname)
 		do_read_version(dev_fd, &fi);
 	}
 
-	if (fi.fd >= 0)
-		close(fi.fd);
-	if (fi.buffer)
-		free(fi.buffer);
+	clean_file(&fi);
 }
 
 /*