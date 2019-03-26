intel/opa-firmware

commit 56f5ca621af766ef4889bebffeadf7b46ad8b04c

hfi1_eprom.c: Add -z to display HFI driver efi version attached to hfi
Adding -z option inservice mode of the tool.
This option will display the version of hfi driver efi which was
attached to each HFI by BIOS during enumeration.

Example Output:

[LINUX ~]# ./a.out -S -z
UEFI driver was attached to following PCI devices
Device: 0000:04:00.0     Driver Version: x.xx.xx.x
Device: 0000:06:00.0     Driver Version: x.xx.xx.x

Update PACKAGE_VERSION to 2

Reviewed-by: Ira Weiny <ira.weiny@intel.com>
Reviewed-by: Kamenee Arumugam <kamenee.arumugam@intel.com>
Signed-off-by: John Fleck <john.fleck@intel.com>
Signed-off-by: Patel Jay P <jay.p.patel@intel.com>

hfi1_eprom.c
@@ -1,5 +1,5 @@
 /*
- * Copyright(c) 2015-2017 Intel Corporation.
+ * Copyright(c) 2015-2018 Intel Corporation.
  *
  * This file is provided under a dual BSD/GPLv2 license.  When using or
  * redistributing this file, you may do so under either license.
@@ -156,6 +156,7 @@ struct file_info {
 #define DO_INFO	   4
 #define DO_VERSION 5
 #define DO_UPDATE  6
+#define DO_EFI_VERSION 7
 
 /* Partitions */
 #define PART_NONE   -2
@@ -1616,6 +1617,50 @@ done:
 	return part;
 }
 
+void get_attached_driver() {
+	FILE *pp;
+	FILE *check_hfi;
+	FILE *Version;
+	int hfi_device = 0;
+	int i,j;
+
+	char *line;
+	char buf[1000];
+	char command[1000];
+
+	printf("HFI UEFI driver was attached to following PCI devices\n");
+	pp = popen("ls /sys/firmware/efi/efivars/*-driver-version-* 2> /dev/null", "r");
+
+	if (pp != NULL) {
+		for(line = fgets(buf, sizeof buf, pp); line != NULL; line = fgets(buf, sizeof buf, pp)) {
+			j= 0; i = 0;
+			while(line[j] != '-') {
+				if( line[j] == '\n') {
+					break;
+				}
+				if( line[j] == '/') {
+					i = j;
+				}
+				j++;
+			}
+			line[j] = '\0';
+			strcpy(command, "lspci -s ");
+			strcat (command,(line + i + 1));
+			strcat (command," | grep HFI");
+			check_hfi = popen(command, "r");
+			if(fgets(command, sizeof command, check_hfi)){
+				strcpy(command, "cat /sys/firmware/efi/efivars/");
+				strcat (command,(line + i + 1));
+				strcat (command,"-driver-version-* | sed -e 's/[^A-Z0-9a-z.]//g'");
+				Version = popen(command, "r");
+				printf("Device: %s \tDriver Version: %s\n", (line + i + 1), fgets(command, sizeof command, Version));
+				pclose(Version);
+			}
+			pclose(check_hfi);
+		}
+	}
+	pclose(pp);
+}
 
 void usage(void)
 {
@@ -1688,6 +1733,7 @@ void usage(void)
 	       "  -i              print the EPROM device ID\n"
 	       "  -s size         override EPROM size, must be power of 2, in Mbits\n"
 	       "  -y              Answer (y)es : Silence warnings and confirmations\n"
+	       "  -z              show actual version of UEFI driver attached to HFIs\n"
 	       "\n");
 }
 
@@ -1700,7 +1746,7 @@ void only_one_operation(void)
 
 void do_error(const char *message)
 {
-	fprintf(stderr, message);
+	fprintf(stderr, "%s", message);
 	usage();
 	exit(1);
 }
@@ -1742,7 +1788,7 @@ int main(int argc, char **argv)
 	 *    that require an argument but don't have one.  Erase and
 	 *    version expect individual options to not have an argument.
 	 */
-	while ((opt = getopt(argc, argv, "-:bcdeShiors:vVwuym")) != -1) {
+	while ((opt = getopt(argc, argv, "-:bcdeShiors:vVwuymz")) != -1) {
 		switch (opt) {
 		case '\1':
 			if(!optarg_dst) {
@@ -1855,6 +1901,9 @@ int main(int argc, char **argv)
 		case 'y':
 			silence_warnings = true;
 			break;
+		case 'z':
+                        operation = DO_EFI_VERSION;
+                        break;
 		case 'S':
 			service_mode = true;
 			break;
@@ -1876,6 +1925,17 @@ int main(int argc, char **argv)
 	if (do_list_devices) {
 		list_all_devices(stdout);
 		exit(0);
+	}
+
+
+	if (operation == DO_EFI_VERSION) {
+		if (service_mode == true) {
+			get_attached_driver();
+			exit(0);
+		} else {
+			usage();
+			exit(0);
+		}
 	}
 
 	if (operation == DO_UPDATE) {

CMakeLists.txt
@@ -54,7 +54,7 @@
 cmake_minimum_required(VERSION 2.6)
 project(opa-firmware C)
 
-set(PACKAGE_VERSION "1")
+set(PACKAGE_VERSION "2")
 
 configure_file (
 	"${PROJECT_SOURCE_DIR}/config.h.in"

commit e32e5e1269b568bfa1c3f949daad2bae5095acab

[kw] Errors in hfi1_eprom tool
There are a number of KW errors in the patch which adds version information to
hfi1eprom tool. Fix those.

Fixes: 56f5ca6 ("hfi1_eprom.c: Add -z to display HFI driver efi version attached to hfi")
Reviewed-by: Patel Jay P <jay.p.patel@intel.com>
Reviewed-by: Ira Weiny <ira.weiny@intel.com>
Signed-off-by: Dennis Dalessandro <dennis.dalessandro@intel.com>

hfi1_eprom.c
@@ -1624,13 +1624,15 @@ void get_attached_driver() {
 	int i,j;
 
 	char *line;
-	char buf[1000];
-	char command[1000];
+	char buf[256];
+	char command[256];
+	char command2[256];
+	char command3[256];
+	char res[256];
 
 	printf("HFI UEFI driver was attached to following PCI devices\n");
 	pp = popen("ls /sys/firmware/efi/efivars/*-driver-version-* 2> /dev/null", "r");
-
-	if (pp != NULL) {
+	if (pp) {
 		for(line = fgets(buf, sizeof buf, pp); line != NULL; line = fgets(buf, sizeof buf, pp)) {
 			j= 0; i = 0;
 			while(line[j] != '-') {
@@ -1644,21 +1646,27 @@ void get_attached_driver() {
 			}
 			line[j] = '\0';
 			strcpy(command, "lspci -s ");
-			strcat (command,(line + i + 1));
-			strcat (command," | grep HFI");
+			strcat(command,(line + i + 1));
+			strcat(command," | grep HFI");
 			check_hfi = popen(command, "r");
-			if(fgets(command, sizeof command, check_hfi)){
-				strcpy(command, "cat /sys/firmware/efi/efivars/");
-				strcat (command,(line + i + 1));
-				strcat (command,"-driver-version-* | sed -e 's/[^A-Z0-9a-z.]//g'");
-				Version = popen(command, "r");
-				printf("Device: %s \tDriver Version: %s\n", (line + i + 1), fgets(command, sizeof command, Version));
-				pclose(Version);
+			if (check_hfi) {
+				if(fgets(command2, sizeof command2, check_hfi)){
+					strcpy(command3, "cat /sys/firmware/efi/efivars/");
+					strcat (command3,(line + i + 1));
+					strcat (command3,"-driver-version-* | sed -e 's/[^A-Z0-9a-z.]//g'");
+					Version = popen(command3, "r");
+					if (Version) {
+						if (fgets(res, sizeof res, Version))
+							printf("Device: %s \tDriver Version: %s\n", (line + i + 1), res);
+						pclose(Version);
+					}
+				}
+				pclose(check_hfi);
 			}
-			pclose(check_hfi);
 		}
+		pclose(pp);
 	}
-	pclose(pp);
+
 }
 
 void usage(void)